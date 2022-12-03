8a6444cc1f8d6f3a1636244f532ce760c0fb614c
Sacha Bartholme <sacha.bartholme@hotmail.com>
2022-12-03 14:27:12 -0500

    improve form validation

--- webapps/burger/forms.py
+++ webapps/burger/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+import string
+allowed = set(string.ascii_lowercase + string.ascii_uppercase + string.digits + '_')
 
 def string_is_valid(s):
-    return s.isalnum()
+    return set(s) <= allowed
+
+def string_is_short(s):
+    return len(s) <= 64
 
 class GameForm(forms.Form):
     room = forms.CharField(max_length = 256)
@@ -10,11 +15,15 @@ class GameForm(forms.Form):
         room = self.cleaned_data.get('room')
         if not string_is_valid(room):
             raise forms.ValidationError("Room name contains invalid characters.")
+        if not string_is_short(room):
+            raise forms.ValidationError("Room name is too long.")
 
         return room
     def clean_username(self):
         username = self.cleaned_data.get('username')
         if not string_is_valid(username):
             raise forms.ValidationError("Username contains invalid characters.")
+        if not string_is_short(username):
+            raise forms.ValidationError("Username is too long.")
         return username
     
