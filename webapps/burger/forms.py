8b76b372b8dd59bfd7b781d5cf8d4289673f8231
Sacha Bartholme <sacha.bartholme@hotmail.com>
2022-12-27 23:06:10 +0100

    remove restrictions on form

--- webapps/burger/forms.py
+++ webapps/burger/forms.py
@@ -1,16 +1,7 @@
 from django import forms
-import string
-allowed = set(string.ascii_lowercase +
-              string.ascii_uppercase + string.digits + '_')
-
-
-def string_is_valid(s):
-    return set(s) <= allowed
-
 
 def string_is_short(s):
     return len(s) <= 64
-
 
 class GameForm(forms.Form):
     room = forms.CharField(max_length=256)
@@ -18,9 +9,6 @@ class GameForm(forms.Form):
 
     def clean_room(self):
         room = self.cleaned_data.get('room')
-        if not string_is_valid(room):
-            raise forms.ValidationError(
-                "Room name contains invalid characters.")
         if not string_is_short(room):
             raise forms.ValidationError("Room name is too long.")
 
@@ -28,9 +16,6 @@ class GameForm(forms.Form):
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
-        if not string_is_valid(username):
-            raise forms.ValidationError(
-                "Username contains invalid characters.")
         if not string_is_short(username):
             raise forms.ValidationError("Username is too long.")
         return username
